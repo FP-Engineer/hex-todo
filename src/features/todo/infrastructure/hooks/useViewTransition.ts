df1ab3496625f41056c182f625322eab3b29fe26
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2025-05-17 22:18:49 +0200

    chore: fixed commitlint

--- src/features/todo/infrastructure/hooks/useViewTransition.ts
+++ src/features/todo/infrastructure/hooks/useViewTransition.ts
@@ -1,11 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { flushSync } from 'react-dom';
 import { useCallback } from 'react';
 
-type ViewTransitionCallback = (...args: unknown[]) => void | Promise<void>;
+type ViewTransitionCallback = (...args: any[]) => void | Promise<void>;
 
 export function useViewTransition(callback: ViewTransitionCallback) {
   return useCallback(
-    (...args: unknown[]) =>
+    (...args: any[]) =>
       document.startViewTransition(() => flushSync(() => callback(...args))),
     [callback]
   );
